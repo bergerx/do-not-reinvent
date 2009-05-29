35bc39989e1a3fba163792b31d71dcf5f2a63da4
bekir <bekir@b67a7fcb-dc60-493a-b9c6-6ad9acd740d3>
2009-05-29 07:03:51 +0000

    Added XVidCap, and committing some old modifications.
    git-svn-id: https://svn.bdgn.net/public/test/bekir/yeniden-kesfetme.d@155 b67a7fcb-dc60-493a-b9c6-6ad9acd740d3

--- tracer.py
+++ tracer.py
@@ -1,8 +1,9 @@
 indent_level = 0
+def tracer(message):
+    print "\033[01;30m%s\033[0;37;00m" % message
+
 def trace(fn):
     def newfn(*args, **kwargs):
-        def color_print(message):
-            print "\033[01;30m%s\033[0;37;00m" % message
         fn_params = ""
         if (args):
             fn_params = ",".join([repr(x) for x in args[1:]])
@@ -10,9 +11,9 @@ def trace(fn):
             fn_params += "," + ",".join(["%s=%s" % (k,repr(v)) for k,v in kwargs])
         globals()["indent_level"] += 1
         indent = globals()["indent_level"] * "\t"
-        color_print("%s > %s(%s)" % (indent, fn.func_name, fn_params))
+        tracer("%s > %s(%s)" % (indent, fn.func_name, fn_params))
         result = fn(*args, **kwargs)
-        color_print("%s < %s result: %s" % (indent, fn.func_name, result))
+        tracer("%s < %s result: %s" % (indent, fn.func_name, result))
         globals()["indent_level"] -= 1
         return result
     return newfn
